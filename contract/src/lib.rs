3e5a7ff610835ae88050114693da12651de8523b
Julian <liannsd3@gmail.com>
2024-02-06 23:45:27 -0300

    Started working on contract

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,19 +1,70 @@
 // Find all our documentation at https://docs.near.org
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::Vector;
 use near_sdk::env::log_str;
+use near_sdk::json_types::U128;
 use near_sdk::near_bindgen;
+use near_sdk::serde::Serialize;
+
+use chrono::{DateTime, Utc};
+
+#[derive(Serialize, BorshDeserialize, BorshSerialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Pokemon {
+    name: String,
+    votes: usize,
+}
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct PokemonContest {
+    id: u64,
+    max_time: U128,
+    poke_first: Pokemon,
+    poke_second: Pokemon,
+}
+
+impl PokemonContest {
+    pub fn new(id: u64, poke_first: Pokemon, poke_second: Pokemon) -> Self {
+        Self {
+            id,
+            poke_first,
+            poke_second,
+            max_time: U128(0),
+        }
+    }
+
+    pub fn vote(&mut self, poke: Pokemon) {
+        if poke.name == self.poke_first.name {
+            self.poke_first.votes += 1;
+            return;
+        } else if poke.name == self.poke_second.name {
+            self.poke_second.votes += 1;
+            return;
+        }
+        panic!("Pokemon not found")
+    }
+
+    pub fn total_votes(&self) -> usize {
+        self.poke_first.votes + self.poke_second.votes
+    }
+}
 
 // Define the contract structure
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
     greeting: String,
+    contests: Vector<PokemonContest>,
 }
 
 // Define the default, which automatically initializes the contract
 impl Default for Contract {
     fn default() -> Self {
-        Self { greeting: "Hello".to_string() }
+        Self {
+            greeting: "Hello".to_string(),
+            contests: Vector::new(b"contests".to_vec()),
+        }
     }
 }
 
@@ -30,6 +81,14 @@ impl Contract {
         log_str(&format!("Saving greeting: {greeting}"));
         self.greeting = greeting;
     }
+    pub fn add_contest(&mut self, id: u64, poke_first: Pokemon, poke_second: Pokemon) {
+        let contest = PokemonContest::new(id, poke_first, poke_second);
+        self.contests.push(&contest);
+    }
+    pub fn vote(&mut self, id: u64, poke: Pokemon) {
+        let mut contest = self.contests.get(id).expect("Contest not found");
+        contest.vote(poke);
+    }
 }
 
 /*
@@ -44,19 +103,13 @@ mod tests {
     fn get_default_greeting() {
         let contract = Contract::default();
         // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(
-            contract.get_greeting(),
-            "Hello".to_string()
-        );
+        assert_eq!(contract.get_greeting(), "Hello".to_string());
     }
 
     #[test]
     fn set_then_get_greeting() {
         let mut contract = Contract::default();
         contract.set_greeting("howdy".to_string());
-        assert_eq!(
-            contract.get_greeting(),
-            "howdy".to_string()
-        );
+        assert_eq!(contract.get_greeting(), "howdy".to_string());
     }
 }
